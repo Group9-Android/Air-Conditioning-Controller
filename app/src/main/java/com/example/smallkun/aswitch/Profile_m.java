56a18dd7ff5312e0390c1e025b42079155b0b59e
CGNz <2289209974@qq.com>
2018-06-07 17:41:23 +0800

    情景模式界面
    完成了模板

--- app/src/main/java/com/example/smallkun/aswitch/Profile_m.java
+++ app/src/main/java/com/example/smallkun/aswitch/Profile_m.java
@@ -1,72 +1,126 @@
 package com.example.smallkun.aswitch;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AnalogClock;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class Profile_m extends AppCompatActivity {
+public class Profile_m extends Activity implements OnClickListener{
 
-    int mYear, mMonth, mDay;
-    Button btn;
-    TextView dateDisplay;
-    final int DATE_DIALOG = 1;
+    private Button btnDate,btnTime,confirmbtn;
+    private TextView tvDate,tvTime;
+    private Spinner spinner1,spinner2;
+    private RadioGroup mode, power;
+    private boolean pd1 = false, pd2=false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        btn = (Button) findViewById(R.id.dateChoose);
-        dateDisplay = (TextView) findViewById(R.id.dateDisplay);
-
-        btn.setOnClickListener(new OnClickListener() {
-
+        setContentView(R.layout.profile);
+        btnDate = (Button) findViewById(R.id.btnDatePick);
+        btnTime = (Button) findViewById(R.id.btnTimePick);
+        tvDate = (TextView) findViewById(R.id.tv_date);
+        tvTime = (TextView) findViewById(R.id.tv_time);
+        btnDate.setOnClickListener(this);
+        btnTime.setOnClickListener(this);
+        spinner1 = (Spinner) findViewById(R.id.wendu);
+        spinner2 = (Spinner) findViewById(R.id.fengsu);
+        confirmbtn = (Button) findViewById(R.id.confirmbtn);
+        mode = (RadioGroup) findViewById(R.id.radioGroup);
+        power = (RadioGroup) findViewById(R.id.radioPower);
+        power.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                showDialog(DATE_DIALOG);
+            public void onCheckedChanged(RadioGroup radioGroup, int checkId) {
+                if (checkId == R.id.radioOff){
+                    spinner1.setEnabled(false);
+                    spinner2.setEnabled(false);
+                    mode.setEnabled(false);
+                } else {
+                    spinner1.setEnabled(true);
+                    spinner2.setEnabled(true);
+                    mode.setEnabled(true);
+                }
             }
         });
 
-        final Calendar ca = Calendar.getInstance();
-        mYear = ca.get(Calendar.YEAR);
-        mMonth = ca.get(Calendar.MONTH);
-        mDay = ca.get(Calendar.DAY_OF_MONTH);
-    }
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DATE_DIALOG:
-                return new DatePickerDialog(this, mdateListener, mYear, mMonth, mDay);
+        List<String> datas = new ArrayList<>();
+        for (int i = 0; i < 13; i++) {
+            datas.add(16 + i+"℃");
         }
-        return null;
-    }
+        //适配器
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,datas);
+        spinner1.setAdapter(adapter);
 
-    /**
-     * 设置日期 利用StringBuffer追加
-     */
-    public void display() {
-        dateDisplay.setText(new StringBuffer().append(mMonth + 1).append("-").append(mDay).append("-").append(mYear).append(" "));
-    }
-
-    private DatePickerDialog.OnDateSetListener mdateListener = new DatePickerDialog.OnDateSetListener() {
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            mYear = year;
-            mMonth = monthOfYear;
-            mDay = dayOfMonth;
-            display();
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            data.add( i+"档");
         }
-    };
+        //适配器
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,data);
+        spinner2.setAdapter(adapter1);
+
+        confirmbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pd1 && pd2){
+                    int selected = mode.getCheckedRadioButtonId();
+                    RadioButton b = Profile_m.this.findViewById(selected);
+
+                    String text = spinner1.getSelectedItem().toString() + "  " + spinner2.getSelectedItem().toString()+"  "+b.getText().toString();
+                    Toast.makeText(Profile_m.this, text, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Profile_m.this, "Please input the Date and Time.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnDatePick:
+                DatePickerDialog datePicker = new DatePickerDialog(this, new OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        tvDate.setText(year+" "+month+" "+day);
+                        pd1= true;
+                    }
+                }, 2018, 2, 12);
+                datePicker.show();
+                break;
+            case R.id.btnTimePick:
+                TimePickerDialog time = new TimePickerDialog(this, new OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        tvTime.setText(hour+":"+minute);
+                        pd2= true;
+                    }
+                }, 18, 25, true);
+                time.show();
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/example/smallkun/aswitch/TwoFragment.java
+++ app/src/main/java/com/example/smallkun/aswitch/TwoFragment.java
@@ -1,6 +1,8 @@
 package com.example.smallkun.aswitch;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,7 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
@@ -18,9 +23,23 @@ import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class TwoFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_two,container,false);
+
+        final Button btntest1 = (Button) view.findViewById(R.id.btntest);
+        btntest1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "2333", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), Profile_m.class);
+                startActivity(intent);
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_two,container,false);
     }
 
@@ -32,6 +51,8 @@ public class TwoFragment extends Fragment {
             "item"
     };
 
+
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -41,5 +62,15 @@ public class TwoFragment extends Fragment {
         View footerView = ((LayoutInflater)activity.getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_footer, null, false);
         listview.addFooterView(footerView);
         listview.setAdapter(adapter);
+
+        final Button btntest1 = (Button) view.findViewById(R.id.btntest);
+        btntest1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "2333", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), Profile_m.class);
+                startActivity(intent);
+            }
+        });
     }
 }
