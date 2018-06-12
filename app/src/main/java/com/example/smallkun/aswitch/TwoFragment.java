78a801fc26b13eb97a8385cba3ae5239dd9b5aae
CGNz <2289209974@qq.com>
2018-06-12 23:55:44 +0800

    情景模式列表UI
    实现布局
    后续添加功能和相应逻辑跳转

--- app/src/main/java/com/example/smallkun/aswitch/TwoFragment.java
+++ app/src/main/java/com/example/smallkun/aswitch/TwoFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,8 @@ public class TwoFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_two,container,false);
     }
 
+    private CardView cardView;
+
     private String[] data = {
             "回家",
             "睡眠",
@@ -36,10 +39,15 @@ public class TwoFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Activity activity = (MainActivity)getActivity();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,android.R.layout.simple_list_item_1,data);
-        ListView listview = (ListView)view.findViewById(R.id.list_view);
-        View footerView = ((LayoutInflater)activity.getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_footer, null, false);
-        listview.addFooterView(footerView);
-        listview.setAdapter(adapter);
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,android.R.layout.simple_list_item_1,data);
+//        ListView listview = (ListView)view.findViewById(R.id.list_view);
+//        View footerView = ((LayoutInflater)activity.getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.list_footer, null, false);
+//        listview.addFooterView(footerView);
+//        listview.setAdapter(adapter);
+
+        cardView = (CardView) view.findViewById(R.id.cardView);
+
+
     }
 }
